ee1c57037e5b0ccfd838d9248ba40f568f5d5474
zoytsa <a.baturskiy@gmail.com>
2021-08-27 00:58:27 +0600

    home

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,10 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) { return MaterialApp( home: MyHomePage(),);
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
   }
 }
 
@@ -30,67 +33,77 @@ class MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(children: [
-        Align(
-          alignment: const Alignment(0, -0.9),
-          child:
-          Text("Test your\nreaction speed", textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.w700,
-                color: Colors.black),
-          ),
-        ),
-        Align(
-          alignment: Alignment.center, child: ColoredBox(
-          color: Colors.black12,
-          child: SizedBox(height: 160, width: 300,
-            child: Center(
-              child: Text(
-                millisecondsText, textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 40,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black),
-              ),
+      backgroundColor: Color(0xFF282E3D),
+      body: Stack(
+        children: [
+          Align(
+            alignment: const Alignment(0, -0.9),
+            child: Text(
+              "Test your\nreaction speed",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 38,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
             ),
           ),
-        ),
-        ),
-        Align(
-          alignment: const Alignment(0, 0.9),
-          child: GestureDetector(
-            onTap: () =>
-                setState(() {
-                  switch (gameState) {
-                    case GameState.readyToStart:
-                      gameState = GameState.waiting;
-                      millisecondsText = "";
-                      startWaitingTimer();
-                      break;
-                    case GameState.waiting:
-                      break;
-                    case GameState.canBeStopped:
-                      gameState = GameState.readyToStart;
-                      stoppableTimer?.cancel();
-                      break;
-                  }
-                }),
+          Align(
+            alignment: Alignment.center,
             child: ColoredBox(
-              color: Colors.black12,
+              color: Color(0xFF6D6D6D),
               child: SizedBox(
-                height: 200, width: 200, child: Center(child: Text(
-                getButtonText(),
-                style: TextStyle(fontSize: 35,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black),
-              ),
-              ),
+                height: 160,
+                width: 300,
+                child: Center(
+                  child: Text(
+                    millisecondsText,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.white),
+                  ),
+                ),
               ),
             ),
           ),
-        ),
-      ],
+          Align(
+            alignment: const Alignment(0, 0.9),
+            child: GestureDetector(
+              onTap: () => setState(() {
+                switch (gameState) {
+                  case GameState.readyToStart:
+                    gameState = GameState.waiting;
+                    millisecondsText = "";
+                    startWaitingTimer();
+                    break;
+                  case GameState.waiting:
+                    break;
+                  case GameState.canBeStopped:
+                    gameState = GameState.readyToStart;
+                    stoppableTimer?.cancel();
+                    break;
+                }
+              }),
+              child: ColoredBox(
+                color: Colors.black12,
+                child: SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: Center(
+                    child: Text(
+                      getButtonText(),
+                      style: TextStyle(
+                          fontSize: 38,
+                          fontWeight: FontWeight.bold,
+                          color: Color(getButtonColor())),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -106,28 +119,37 @@ class MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  int getButtonColor() {
+    switch (gameState) {
+      case GameState.readyToStart:
+        return 0xFF40CA88;
+      case GameState.waiting:
+        return 0xFFE0982D;
+      case GameState.canBeStopped:
+        return 0xFFE02D47;
+    }
+  }
+
   void startWaitingTimer() {
     final int randomMilliseconds = Random().nextInt(4000) + 1000;
-    waitingTimer = Timer(
-        Duration(milliseconds: randomMilliseconds),
-            () {
-          setState(() {
-            gameState = GameState.canBeStopped;
-          });
-          _startStoppableTimer();
-        });
+    waitingTimer = Timer(Duration(milliseconds: randomMilliseconds), () {
+      setState(() {
+        gameState = GameState.canBeStopped;
+      });
+      _startStoppableTimer();
+    });
   }
 
   void _startStoppableTimer() {
-    stoppableTimer = Timer.periodic(Duration(milliseconds: 16),
-            (timer) {
-          setState(() {
-            millisecondsText = "${timer.tick * 16} ms";
-          });
-        });
+    stoppableTimer = Timer.periodic(Duration(milliseconds: 16), (timer) {
+      setState(() {
+        millisecondsText = "${timer.tick * 16} ms";
+      });
+    });
   }
 
-  @override void dispose() {
+  @override
+  void dispose() {
     waitingTimer?.cancel();
     stoppableTimer?.cancel();
     super.dispose();
